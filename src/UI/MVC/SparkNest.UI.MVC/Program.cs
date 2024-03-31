fbed938baeb7f25ae7c11f06f9ab7fa6e04c7892
RasulPirsotanov1 <7m0uvuf@code.edu.az>
2024-03-31 22:44:22 +0400

    Added Admin area

--- src/UI/MVC/SparkNest.UI.MVC/Program.cs
+++ src/UI/MVC/SparkNest.UI.MVC/Program.cs
@@ -80,8 +80,16 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+
+app.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
 
 app.Run();
